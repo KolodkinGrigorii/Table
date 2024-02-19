29b7299f089bb12b49c422fb50b29424f0f844e2
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2024-02-19 19:10:31 +0300

    Table

--- table/table/source.cpp
+++ table/table/source.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 int main() {
 	Table<string, string> t;
+	int count = 0;
 	t.insert("first", "abc");
 	t.insert("second", "cba");
-	cout << t << endl;
-	t.remove("second");
-	cout << t;
-	cout << t["first"];
+	t.insert("third", "bca");
+	for (auto it = t.find("second"); it != t.end(); it++) {
+		count++;
+	}
+	cout << count;
 }
