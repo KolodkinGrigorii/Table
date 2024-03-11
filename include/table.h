c1a857ef6974222fd5711f393753386460d9c0d8
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2024-03-11 18:33:44 +0300

    tables

--- include/table.h
+++ include/table.h
@@ -1,10 +1,12 @@
+#pragma once
+
 #include <iostream>
 #include <vector>
 #include <iterator>
 using namespace std;
 template <typename TypeKey, typename TypeData>
 class Table {
-private:
+protected:
 	vector<pair<TypeKey, TypeData>> ar;
 public:
 	Table() {
@@ -18,34 +20,22 @@ public:
 			ar.push_back(make_pair(key, data));
 		}
 	}
-	Table<TypeKey, TypeData> operator=(const TypeKey& t) {
-		while (ar.size() != 0) {
-			ar.pop_back();
-		}
-		auto iter = t.ar.begin();
-		for (auto it = t.ar.begin(); it != t.ar.end(); it++) {
-			TypeKey key = it->first;
-			TypeData data = it->second;
-			ar.push_back(make_pair(key, data));
-		}
+	// TODO: podsazka Table a(20); a=a; // ????
+	Table<TypeKey, TypeData>& operator=(const Table<TypeKey, TypeData>& t) {
+		ar = t.ar;
 		return *this;
 	}
 	~Table() {
-		int size = ar.size();
-		while (size != 0) {
-			ar.pop_back();
-			size = ar.size();
-		}
+		ar.clear();
 	}
 	auto insert(const TypeKey& key, const TypeData& data) {
 		ar.push_back(make_pair(key, data));
 		auto iter = ar.begin();
 		for (auto it = ar.begin(); it != ar.end(); it++) {
 			if (it->first == key) {
-				iter = it;
+				return it;
 			}
 		}
-		return iter;
 	}
 	auto find(const TypeKey& key) {
 		if (ar.size() == 0) {
@@ -54,13 +44,10 @@ public:
 		auto iter = ar.begin();
 		for (auto it = ar.begin(); it != ar.end(); it++) {
 			if (it->first == key) {
-				iter = it;
-			}
-			if (it->first != key && (it == (ar.end()-= 1))) {
-				return ar.end();
+				return it;
 			}
 		}
-		return iter;
+		return ar.end();
 	}
 	auto end() {
 		auto iter = ar.end();
@@ -79,12 +66,14 @@ public:
 		}
 		return false;
 	}
-	TypeData operator [] (const TypeKey& key) {
+	// TODO: test for TypeData&
+	TypeData& operator [] (const TypeKey& key) {
 		for (auto it = ar.begin(); it != ar.end(); it++) {
 			if (it->first == key) {
 				return it->second;
 			}
 		}
+		throw 'FALL';
 	}
 	friend std::ostream& operator<<(std::ostream& out, const Table& v) {
 		for (auto it = v.ar.begin(); it != v.ar.end(); it++) {
@@ -95,61 +84,29 @@ public:
 };
 template <typename TypeKey, typename TypeData>
 class SortTable: public Table<TypeKey,TypeData> {
-private:
-	vector<pair<TypeKey, TypeData>> sortar;
 public:
-    SortTable() {
-		vector<pair<TypeKey, TypeData>> sortar;
-	}
-	SortTable(const SortTable& t) {
-		auto iter = t.sortar.begin();
-		for (auto it = t.sortar.begin(); it != t.sortar.end(); it++) {
-			TypeKey key = it->first;
-			TypeData data = it->second;
-			sortar.push_back(make_pair(key, data));
-		}
-	}
-	SortTable<TypeKey, TypeData> operator=(const TypeKey& t) {
-		while (sortar.size() != 0) {
-			sortar.pop_back();
-		}
-		auto iter = t.sortar.begin();
-		for (auto it = t.sortar.begin(); it != t.ar.end(); it++) {
-			TypeKey key = it->first;
-			TypeData data = it->second;
-			ar.push_back(make_pair(key, data));
-		}
-		return *this;
-	}
-	~SortTable() {
-		int size = sortar.size();
-		while (size != 0) {
-			sortar.pop_back();
-			size = sortar.size();
-		}
-	}
 	auto insert(const TypeKey& key, const TypeData& data) {
-		if (sortar.size() == 0) {
-			sortar.push_back(make_pair(key, data));
-			return sortar.begin();
+		if (ar.size() == 0) {
+			ar.push_back(make_pair(key, data));
+			return ar.begin();
 		}
 		else {
 			int left = 0;
-			int right = sortar.size()-1;
-			auto pos=sortar.begin();
-			if (key >= sortar[right].first) {
-				sortar.push_back(make_pair(key, data));
-				return sortar.end()-=1;
+			int right = ar.size()-1;
+			auto pos=ar.begin();
+			if (key >= ar[right].first) {
+				ar.push_back(make_pair(key, data));
+				return ar.end()-=1;
 			}
-			else if (key <= sortar[left].first) {
-				sortar.insert(sortar.begin(), make_pair(key, data));
-				return sortar.begin();
+			else if (key <= ar[left].first) {
+				ar.insert(ar.begin(), make_pair(key, data));
+				return ar.begin();
 			}
 			else {
 				int mid;
 				while (right - left != 1) {
 					mid = (right + left) / 2;
-					if (key >= sortar[mid].first) {
+					if (key >= ar[mid].first) {
 						left = mid;
 					}
 					else {
@@ -157,38 +114,38 @@ public:
 					}
 				}
 				pos += right;
-				return sortar.insert(pos, make_pair(key, data));
+				return ar.insert(pos, make_pair(key, data));
 			}
 		}
 	}
 	auto find(const TypeKey& key) {
-		if (sortar.size() == 0 || key<sortar.begin()->first 
-			|| key> (sortar.end()-=1)->first) {
-			return sortar.end();
+		if (ar.size() == 0 || key<ar.begin()->first 
+			|| key> (ar.end()-=1)->first) {
+			return ar.end();
 		}
 		else {
 			int left = 0;
-			int right = sortar.size() - 1;
-			auto pos = sortar.begin();
-			if (key == sortar[right].first) {
-				return sortar.end()-=1;
+			int right = ar.size() - 1;
+			auto pos = ar.begin();
+			if (key == ar[right].first) {
+				return ar.end()-=1;
 			}
-			else if (key == sortar[left].first) {
+			else if (key == ar[left].first) {
 				return pos;
 			}
 			else {
 				int mid = left;
-				while (sortar[mid].first != key && right-left!=1) {
+				while (ar[mid].first != key && right-left!=1) {
 					mid = (right + left) / 2;
-					if (key > sortar[mid].first) {
+					if (key > ar[mid].first) {
 						left = mid;
 					}
-					else if (key<sortar[mid].first) {
+					else if (key<ar[mid].first) {
 						right = mid;
 					}
 				}
-				if (sortar[mid].first != key) {
-					return sortar.end();
+				if (ar[mid].first != key) {
+					return ar.end();
 				}
 				else {
 					pos += mid;
@@ -197,32 +154,8 @@ public:
 			}
 		}
 	}
-	auto end() {
-		auto iter = sortar.end();
-		return iter;
-	}
-	auto begin() {
-		auto iter = sortar.begin();
-		return iter;
-	}
-	bool remove(const TypeKey& key) {
-		for (auto it = sortar.begin(); it != sortar.end(); it++) {
-			if (it->first == key) {
-				sortar.erase(it);
-				return true;
-			}
-		}
-		return false;
-	}
-	TypeData operator [] (const TypeKey& key) {
-		for (auto it = sortar.begin(); it != sortar.end(); it++) {
-			if (it->first == key) {
-				return it->second;
-			}
-		}
-	}
 	friend std::ostream& operator<<(std::ostream& out, const SortTable& v) {
-		for (auto it = v.sortar.begin(); it != v.sortar.end(); it++) {
+		for (auto it = v.ar.begin(); it != v.ar.end(); it++) {
 			cout << it->first << " " << it->second << endl;
 		}
 		return out;
